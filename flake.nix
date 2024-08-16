c2112f6bccd7eee0369a3debfaabaaaf4656b063
Jan Stranik <jan@stranik.org>
2024-08-16 16:45:46 -0400

    Fix flake

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         server = pkgs.callPackage ./server/fix-server.nix {};
         fix-client = pkgs.callPackage ./client/fix-client.nix {};
         fix-web-nix = pkgs.callPackage ./web/fix-web.nix { inherit (server) work_dir; };
-      in {
+      in rec {
         packages = rec {
           inherit fix-client;
           inherit (fix-web-nix) fix-web fix-web-starter;
